e5e09e4c9510409281a8f8e53a0a7f0e1cbec885
Clement BEAURAIN <clementbeaurainpro@gmail.com>
2022-06-13 17:50:30 +0200

    fin de journee

--- headers/MessageBuilder.hpp
+++ headers/MessageBuilder.hpp
@@ -9,7 +9,7 @@
 # define RPL_CREATED(date) ("This server was created " + date)
 # define RPL_MYINFO(servername, version, usr_mod, chan_mod) (servername + " " + version + " " + usr_mod + " " + chan_mod)
 # define RPL_UMODEIS(usr_mod) (usr_mod)
-# define RPL_NONICKNAMEGIVEN ":No nickname given"
+# define ERR_NONICKNAMEGIVEN ":No nickname given"
 # define ERR_ERRONEUSNICKNAME (nick) nick + " :Erroneous nickname"
 # define ERR_NICKNAMEINUSE (nick) nick + " :Nickname is already in use"
 # define ERR_UNAVAILRESOURCE (nick) nick + " :Nick is temporarily unavailable"
--- srcs/MessageBuilder.cpp
+++ srcs/MessageBuilder.cpp
@@ -18,6 +18,8 @@ std::string    find_reply(int code, std::string arg1, std::string arg2, std::str
             return RPL_UMODEIS(arg1);
         case 409:
             return ERR_NOORIGIN;
+        case 431:
+            return ERR_NONICKNAMEGIVEN;
         case 461:
             return ERR_NEEDMOREPARAMS(arg1);
         case 462:
