2cfc4a404cdfa4c59ccce40d2adf7d2a2d1809fe
Clement BEAURAIN <clementbeaurainpro@gmail.com>
2022-06-13 15:13:59 +0200

    PASS fonctionne mais les ERR pas test

--- srcs/Commands/pass.cpp
+++ srcs/Commands/pass.cpp
@@ -2,9 +2,23 @@
 
 int		pass(const std::string &line, int fd, Server& server)
 {
-	(void)line;
-	(void)fd;
-	(void)server;
-	COUT "Pointeur pass fonction" ENDL;
-	return 2;
+	COUT "Point pass fonction" ENDL;
+	std::vector<std::string>parsedLine = ft_split(line, ' ');
+	if (parsedLine.size() < 2)
+	{
+		server.send_reply(fd, 461, "PASS", "", "", "");
+		return 1;		
+	}
+	if (server.findMatchingUser(fd) != NULL)
+	{
+		server.send_reply(fd, 462, "", "", "", "");
+		return 1;
+	}
+	if (parsedLine[1] != server.getServerPassword())
+	{
+		DEB "Password " << parsedLine[1] << " was wrong" ENDL;
+		return 1;
+	}
+	DEB "Pass is matching" ENDL;
+	return 0;
 }
