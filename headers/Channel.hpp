7da600fa3a7bfc787202cad18b0a3382fb86345f
Samy CHOUAM <sachouam@e3r3p4.clusters.42paris.fr>
2022-06-16 22:25:21 +0200

    Channel.cpp : add getters and setters

--- headers/Channel.hpp
+++ headers/Channel.hpp
@@ -3,6 +3,18 @@
 #include "Irc.hpp"
 #include "Server.hpp"
 
+#define ANONYMOUS "a"
+#define INVITE_ONLY "i"
+#define MODERATED "m"
+#define NO_MESSAGE "n"
+#define QUIET "q"
+#define PRIVATE "p"
+#define SECRET "s"
+#define REOP "r"
+#define TOPIC "t"
+#define USER_LIMIT "l"
+#define KEY "k"
+
 class Channel
 {
 	public:
@@ -14,24 +26,24 @@ class Channel
     int							_socket;
     std::string             	_name;
     std::string             	_topic;
-    std::string             	_password;
+    std::string             	_key;
     std::string	            	_mode;
+    std::string                 _flags;
     Clients      				_clients;
 
     public:
 
-    Channel();
-    Channel(std::string name, std::string password = std::string());
+    Channel(std::string name, std::string key = std::string());
     virtual ~Channel();
 
     const std::string&			getName(void) const;
     const std::string&			getTopic(void) const;
-    const std::string&			getPassword(void) const;
+    const std::string&			getKey(void) const;
     const std::string&			getMode(void) const;
 
     void						setName(std::string const & name);
 	void						setTopic(std::string const & topic);
-    void						setPassword(std::string const & psw);
-    void						addMode(std::string const & mode);
-
+    void						setKey(std::string const & key);
+    void                        addMode(std::string const mode);
+    void                        delMode(std::string const mode);
 };
--- srcs/Channel.cpp
+++ srcs/Channel.cpp
@@ -1,13 +1,10 @@
 #include "../headers/Channel.hpp"
 
-Channel::Channel()
+Channel::Channel(std::string name, std::string key = std::string()) :
+	_name(name), _topic(std::string()), _key(key), _mode(std::string()), _flags("aimnqpsrtlk")
 {
-	if (DEBUG == 2)
-		COUT "Construct Channel " << this->_name ENDL;
-}
-
-Channel::Channel(std::string name) : _name(name)
-{
+	if (_key != std::string())
+		this->addMode(KEY);
 	if (DEBUG == 2)
 		COUT "Construct Channel " << this->_name ENDL;
 }
@@ -15,4 +12,65 @@ Channel::Channel(std::string name) : _name(name)
 Channel::~Channel()
 {
 	COUT "Destruct Channel." ENDL;
+}
+
+const std::string&			Channel::getName(void) const
+{
+	return _name;
+}
+
+const std::string&			Channel::getTopic(void) const
+{
+	return _topic;
+}
+
+const std::string&			Channel::getKey(void) const
+{
+	return _key;
+}
+
+const std::string&			Channel::getMode(void) const
+{
+	return _mode;
+}
+
+void						Channel::setName(std::string const & name)
+{
+	_name = name;
+}
+
+void						Channel::setTopic(std::string const & topic)
+{
+	_topic = topic;
+}
+
+void						Channel::setKey(std::string const & key)
+{
+	_key = key;
+}
+
+void						Channel::addMode(std::string mode)
+{
+	for (std::string::iterator it = mode.begin(); it != mode.end(); ++it)
+	{
+		// if (*it == 'o' && *it == 'O')
+			// continue ;
+		if (_mode.empty())
+			_mode = *it;
+		else if (_mode.find(*it) == std::string::npos)
+			_mode += *it;
+	}
+}
+
+void						Channel::delMode(std::string mode)
+{
+	if (_mode.empty())
+		return;
+	for (std::string::iterator it = mode.begin(); it != mode.end(); ++it)
+	{
+		// if (*it == 'a' && *it != 'r')
+			// continue ; 
+		if (mode.find(*it) != std::string::npos)
+			_mode.erase(it);
+	}
 }
