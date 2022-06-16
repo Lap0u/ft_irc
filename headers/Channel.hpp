7da506d8e418a727c580e799bad12faaac83da87
Samy CHOUAM <sachouam@e3r3p4.clusters.42paris.fr>
2022-06-16 16:12:33 +0200

    setters and getters in channel.hpp

--- headers/Channel.hpp
+++ headers/Channel.hpp
@@ -5,17 +5,33 @@
 
 class Channel
 {
+	public:
+
+	typedef Server::userVector	Clients;
+
     private:
     
-    int						_socket;
-    std::string             _name;
-    std::string	            _mode;
-    Server::userVector      _clients;
+    int							_socket;
+    std::string             	_name;
+    std::string             	_topic;
+    std::string             	_password;
+    std::string	            	_mode;
+    Clients      				_clients;
 
     public:
 
     Channel();
-    Channel(std::string name);
+    Channel(std::string name, std::string password = std::string());
     virtual ~Channel();
+
+    const std::string&			getName(void) const;
+    const std::string&			getTopic(void) const;
+    const std::string&			getPassword(void) const;
+    const std::string&			getMode(void) const;
+
+    void						setName(std::string const & name);
+	void						setTopic(std::string const & topic);
+    void						setPassword(std::string const & psw);
+    void						addMode(std::string const & mode);
 
 };
