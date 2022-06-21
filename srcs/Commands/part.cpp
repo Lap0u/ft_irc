af28b6cc2b7919fad830c6bd8fa917f6327dc418
Claken <samychouam@gmail.com>
2022-06-21 15:52:59 +0200

    part done

--- srcs/Commands/part.cpp
+++ srcs/Commands/part.cpp
@@ -9,20 +9,20 @@
 
 int     part(const std::string &line, int fd, Server& server)
 {
-    std::vector<std::string> tab = ft_split(line, ' ');
+    std::vector<std::string>		tab = ft_split(line, ' ');
 	if (tab.size() == 1)
 	{
 		server.send_reply(fd, P_ERR_NEEDMOREPARAMS, "part:", ES, ES, ES);
 		return 1;
 	}
-	std::vector<std::string> tab1 = ft_split(tab[1].data(), ',');
 	if (tab.size() >= 2 && tab.size() <= 3)
 	{
+		std::vector<std::string> 	tab1 = ft_split(tab[1].data(), ',');
+		User*						client = server.findMatchingUser(fd);
+		Channel*					chan;
+		std::string					message;
 		for (unsigned int i = 0; i < tab1.size(); i++)
 		{
-			Channel*	chan;
-			User*		client;
-			std::string message;
 
 			if ((chan = server.findChannel(tab1[i])) == NULL)
 			{
@@ -30,7 +30,6 @@ int     part(const std::string &line, int fd, Server& server)
 			}
 			else
 			{
-				client = server.findMatchingUser(fd);
 				if (chan->partWithAClient(client->getNick()) == 1)
 				{
 					server.send_reply(fd, P_ERR_NOTONCHANNEL, chan->getName(), ES, ES, ES);
@@ -38,7 +37,7 @@ int     part(const std::string &line, int fd, Server& server)
 				else
 				{
 					message = tab.size() == 3 ? tab[2] : client->getNick();
-					server.send_raw_message(fd, message);
+					server.send_raw_message(fd, " " + message);
 				}
 			}
 		}
