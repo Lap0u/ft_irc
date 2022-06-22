01efdf81a323d8b5a97eea1c5aa451117e9e8ab9
Samy CHOUAM <sachouam@e3r4p5.42.fr>
2022-06-22 17:23:22 +0200

    join error checking changed

--- srcs/Commands/join.cpp
+++ srcs/Commands/join.cpp
@@ -50,7 +50,6 @@ void	joinChannel_and_send_replies(int fd, Server& server, std::string& chaname, 
 	if (joined == 0)
 	{
 		server.send_chan_message(user, "JOIN", chaname, ES);
-		COUT "On repond JOIN" ENDL;
 		if (chan->getTopic() != ES)
 			server.send_reply(fd, J_RPL_TOPIC, chan->getName(), chan->getTopic(), ES, ES);
 		server.parseCmd("NAMES " + chan->getName(), fd);
