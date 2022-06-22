d91989ba4e96280f60d074e2ef7940eabd7090b0
Clement BEAURAIN <clementbeaurainpro@gmail.com>
2022-06-22 17:01:28 +0200

    remis comme il faut, tout marche

--- srcs/socket.cpp
+++ srcs/socket.cpp
@@ -105,7 +105,7 @@ void    launch_serv(std::string port, std::string password)
 	std::string	separatedline;
 	int			fd;
 	
-	test_reply(1, server);
+	// test_reply(1, server);
 	while(1)
 	{
 		ret_poll = poll(server.getSocket(0), server.getSocketSize(), 15000);
